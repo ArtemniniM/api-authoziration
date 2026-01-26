1bfb97ef771cbf48e19fbe3aecc3b201106cf97d
Artem Minin <artemminin.03@mail.ru>
2026-01-26 17:13:35 +0300

    add token

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const { addUser, authUser } = require('../servicies/api.service');
 const { bodyCheckAuth, bodyCheckUserInfo } = require('../helper/middlewares');
 
@@ -15,7 +16,10 @@ router.post('/reg', bodyCheckAuth, bodyCheckUserInfo, async (req, res) => {
 router.post('/auth', bodyCheckAuth, async (req, res) => {
   try {
     const { mail, password } = req.body;
-    res.status(200).send(await authUser(mail, password));
+    const result = await authUser(mail, password);
+    const token = jwt.sign(result[0], 'sdfjln23jn24j32nkrhb45bhj35v24uio1jxopp431');
+    res.setHeader('Authorization', `Bearer ${token}`);
+    res.status(200).send(result);
   } catch (error) {
     res.status(404).send(error.message);
   }
